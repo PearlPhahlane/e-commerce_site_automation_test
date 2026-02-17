04e88e9e2060aedd9b418333981dd38d4fd45e5e
Pearl Phahlane <pearlphahlane@192.168.1.16>
2026-02-17 21:13:48 +0200

    fix code errors for tests to pass

--- tests/saucedemo.spec.js
+++ tests/saucedemo.spec.js
@@ -46,3 +46,35 @@ test("User can add item to cart", async ({page}) => {
 
 });
 
+test("User can checkout", async ({page}) =>{
+    await page.goto("https://www.saucedemo.com/");
+
+    //Login
+    await page.fill('[data-test="username"]', "standard_user");
+    await page.fill('[data-test="password"]', "secret_sauce");
+    await page.click('[data-test="login-button"]');
+
+    //Add item to cart 
+    await page.click('[data-test="add-to-cart-sauce-labs-backpack"]');
+
+    //Go to cart
+    await page.click('.shopping_cart_link');
+
+    //Checkout
+    await page.click('[data-test="checkout"]');
+
+    //Fill in checkout info
+    await page.fill('[data-test="firstName"]', "Pearl");
+    await page.fill('[data-test="lastName"]', "Phahlane");
+    await page.fill('[data-test="postalCode"]', "8859 Sefatsa Street, Sharpeville, 1928");
+
+    //Continue to overview
+    await page.click('[data-test="continue"]');
+
+    //Finish checkout
+    await page.click('[data-test="finish"]');
+
+    //Confirm checkout success
+    await expect(page.locator('.complete-header')).toHaveText("Thank you for your order!");
+
+});
