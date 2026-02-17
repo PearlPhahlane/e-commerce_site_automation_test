a96e695ccd2a17bf956fcdee699ce28562da17f1
Pearl Phahlane <pearlphahlane@192.168.1.16>
2026-02-17 20:54:52 +0200

    add chect to test checkout

--- tests/saucedemo.spec.js
+++ tests/saucedemo.spec.js
@@ -26,3 +26,23 @@ test("User can login", async({page}) => {
     await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
 });
 
+test("User can add item to cart", async ({page}) => {
+    await page.goto("https://www.saucedemo.com/");
+
+    //Login
+    await page.fill('[data-test="username"]', "standard_user");
+    await page.fill('[data-test="password"]', "secret_sauce");
+    await page.click('[data-test="login-button"]');
+
+    //Add item to cart 
+    await page.click('[data-test="add-to-cart-sauce-labs-backpack"]');
+
+    //Go to cart
+    await page.click('.shopping_cart_link');
+
+    //Confirm item is in cart
+    await expect(page.locator('.cart_item')).toContainText("Sauce Labs Backpack");
+
+
+});
+
