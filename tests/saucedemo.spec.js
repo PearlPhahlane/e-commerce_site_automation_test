f32730ca5a61bb8dc454fe1cca71c0b4244b3425
Pearl Phahlane <pearlphahlane@192.168.1.16>
2026-02-17 20:42:48 +0200

    add check to test if item can be located in the cart

--- tests/saucedemo.spec.js
+++ tests/saucedemo.spec.js
@@ -5,4 +5,24 @@ test("SauceDemo homepage loads", async ({ page }) => {
 
     //Check login button is visible
     await expect(page.locator('[data-test="login-button"]')).toBeVisible();
+
 });
+
+test("User can login", async({page}) => {
+
+    await page.goto("https://www.saucedemo.com/");
+    
+    //Check login button is visible
+    await expect(page.locator('[data-test="login-button"]')).toBeVisible();
+
+    //Enter username and password
+    await page.fill('[data-test="username"]', "standard_user");
+    await page.fill('[data-test="password"]', "secret_sauce");
+
+    //Click login button
+    await page.click('[data-test="login-button"]');
+
+    //Confirm login success 
+    await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+});
+
